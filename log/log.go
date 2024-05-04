13fae2e3aeeaf5f6d14d33532de318e4b1b90ab8
naren mudivarthy <naren.mudivarthy@gmail.com>
2024-05-04 14:44:01 -0700

    Rename dhandler

--- log/log.go
+++ log/log.go
@@ -8,51 +8,51 @@ import (
 )
 var th = slog.NewTextHandler(os.Stdout, nil)
 
-var Logger = slog.New(NewLevelHandler(slog.LevelDebug, th))
+var Logger = slog.New(NewHandler(slog.LevelDebug, th))
 
 
 var AddTracingContext = otelslog.AddTracingContext
 
-// A LevelHandler wraps a Handler with an Enabled method
+// A Handler wraps a Handler with an Enabled method
 // that returns false for levels below a minimum.
-type LevelHandler struct {
+type Handler struct {
 	level   slog.Leveler
 	handler slog.Handler
 }
 
-// NewLevelHandler returns a LevelHandler with the given level.
+// NewHandler returns a LevelHandler with the given level.
 // All methods except Enabled delegate to h.
-func NewLevelHandler(level slog.Leveler, h slog.Handler) *LevelHandler {
+func NewHandler(level slog.Leveler, h slog.Handler) *Handler {
 	// Optimization: avoid chains of LevelHandlers.
-	if lh, ok := h.(*LevelHandler); ok {
+	if lh, ok := h.(*Handler); ok {
 		h = lh.Handler()
 	}
-	return &LevelHandler{level, h}
+	return &Handler{level, h}
 }
 
 // Enabled implements Handler.Enabled by reporting whether
 // level is at least as large as h's level.
-func (h *LevelHandler) Enabled(_ context.Context, level slog.Level) bool {
+func (h *Handler) Enabled(_ context.Context, level slog.Level) bool {
 	return level >= h.level.Level()
 }
 
 // Handle implements Handler.Handle.
-func (h *LevelHandler) Handle(ctx context.Context, r slog.Record) error {
+func (h *Handler) Handle(ctx context.Context, r slog.Record) error {
 	slog.Info(r.Message, "level", r.Level, "name", "naren")
 	return h.handler.Handle(ctx, r)
 }
 
 // WithAttrs implements Handler.WithAttrs.
-func (h *LevelHandler) WithAttrs(attrs []slog.Attr) slog.Handler {
-	return NewLevelHandler(h.level, h.handler.WithAttrs(attrs))
+func (h *Handler) WithAttrs(attrs []slog.Attr) slog.Handler {
+	return NewHandler(h.level, h.handler.WithAttrs(attrs))
 }
 
 // WithGroup implements Handler.WithGroup.
-func (h *LevelHandler) WithGroup(name string) slog.Handler {
-	return NewLevelHandler(h.level, h.handler.WithGroup(name))
+func (h *Handler) WithGroup(name string) slog.Handler {
+	return NewHandler(h.level, h.handler.WithGroup(name))
 }
 
 // Handler returns the Handler wrapped by h.
-func (h *LevelHandler) Handler() slog.Handler {
+func (h *Handler) Handler() slog.Handler {
 	return h.handler
 }
